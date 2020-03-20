3c45b5148937e457fe5e94ec97ecb0b6ae179e64
Ankit Yadav <ankitjnyadav@gmail.com>
2020-03-20 22:45:19 +0530

    Interpolation

--- RedPy/menu.py
+++ RedPy/menu.py
@@ -10,8 +10,27 @@ Press 3: To Configure Web Server
 Press 4: To Create file
 Press 5: To Create User
 Press 6: To configure n/w
-Press 7: To Exit!
-Please Enter your choice:''',end=' ')
+Press 7: To Exit!''')
 print("-"*80)
+print('Please Enter your choice:',end=' ')
+choice = int(input())
+print("-"*80, end='\n')
 
-choice = input()
+if choice==1:
+    os.system("date")
+elif choice==2:
+    os.system("cal")
+elif choice==3:
+    os.system("yum install httpd")
+elif choice==4:
+    print('Choice 4')
+elif choice==5:
+    user_name = input('Enter the username: ')
+    os.system("useradd {}".format(user_name))
+elif choice==6:
+    os.system("date")
+elif choice==7:
+    os.system("date")
+else:
+    print('Option not available')
+print("-"*80, end='\n')
